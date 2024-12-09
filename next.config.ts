1d7b406c179b8200e3ed82ceb24ed2de2afc4f3c
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2024-12-09 13:06:19 +0100

    add necessary env variables

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   eslint: { ignoreDuringBuilds: true },
   experimental: {
     optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
