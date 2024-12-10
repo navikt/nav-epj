f004ea8fde6856f72abe1a39ba6a6802c991e108
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2024-12-10 11:32:11 +0100

    remove cookie

--- src/components/patient-picked.tsx
+++ src/components/patient-picked.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React from 'react'
+import { cookies } from 'next/headers'
+import { revalidatePath } from 'next/cache'
 
 const PatientPicked = async () => {
-  return (
-    <div className="w-full h-full flex flex-col">
-      <h3 className="p-2 font-bold">Patient</h3>
-      <div className="text-xs text-gray-200">Active consultation</div>
-      <div >TODO FNR</div>
-      <div className="text-xs text-gray-200">Name</div>
-      <div >TODO NAME</div>
+    return (
+        <div className="w-full h-full flex flex-col">
+            <h3 className="p-2 font-bold">Patient</h3>
+            <div className="text-xs text-gray-200">Active consultation</div>
+            <div>TODO FNR</div>
+            <div className="text-xs text-gray-200">Name</div>
+            <div>TODO NAME</div>
 
-      <button className="mt-3 p-2 bg-red-500 text-white rounded hover:bg-red-600">
-        Close consultation
-      </button>
-    </div>
-  );
-};
+            <button
+                className="mt-3 p-2 bg-red-500 text-white rounded hover:bg-red-600"
+                onClick={async () => {
+                    'use server'
+                    const cookieStore = await cookies()
+                    cookieStore.delete('patient-id')
+                    revalidatePath('/')
+                }}
+            >
+                Close consultation
+            </button>
+        </div>
+    )
+}
 
-export default PatientPicked;
+export default PatientPicked
