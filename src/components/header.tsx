cb073d07e89e35720228a17174ff616792b3e467
Karl J. Overå <k@rl.run>
2024-12-16 12:32:47 +0100

    chore: update logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Heading } from '@navikt/ds-react'
+import Image from 'next/image'
+
+import icon from '../app/favicon.ico'
 
 const Header = () => {
     return (
         <div className=" w-full flex items-center justify-between">
             <div className="p-2 flex">
-                <div className="text-3xl p-2 mr-2">🔥</div>
+                <div className="text-3xl p-2 -mt-0.5">
+                    <Image src={icon} alt="" width="42" height="42" unoptimized />
+                </div>
                 <div>
                     <Heading level="1" size="medium">
-                        Nav EPJs
+                        Nav Test EPJ
                     </Heading>
-                    <div className="text-xs">Et fake journalsystem med testdata fra Dolly</div>
+                    <div className="text-xs">Et fake pasientjournalsystem med testdata fra Dolly</div>
                 </div>
             </div>
             <div className="p-2 text-right">
