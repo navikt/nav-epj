8794c28d50488ebfa7e2289f78931f9fb8107a8b
Karl J. Overå <k@rl.run>
2026-02-25 16:34:45 +0100

    chore: json logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation(libs.ktor.server.cors)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
+    implementation(libs.logback.encoder)
     implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
