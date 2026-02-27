4c0de79f539184c99e4f87ac289cc93c24e89c7d
Karl J. Overå <k@rl.run>
2026-02-27 09:29:35 +0100

    chore: add debug user endpoint

---
+++ src/main/kotlin/frontend/user/User.kt
@@ -0,0 +1,24 @@
+package frontend.user
+
+import io.ktor.server.auth.principal
+import io.ktor.server.routing.RoutingContext
+
+data class User(
+    val hpr: String,
+)
+
+data class HelseIdPrincipal(
+    val user: User,
+    val debug: DebugInfo
+)
+
+data class DebugInfo(
+    val accessToken: String,
+    val idToken: String,
+)
+
+fun RoutingContext.loggedInUser(): User {
+    val principal = requireNotNull(this.call.principal<HelseIdPrincipal>()) { "User not found in principal" }
+
+    return principal.user
+}
