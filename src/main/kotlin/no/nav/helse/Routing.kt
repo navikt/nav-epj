977dbde9ac876044941ac362c81c828e745e15e1
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2026-05-06 14:31:13 +0200

    fix: add token subject to welcome message

--- src/main/kotlin/no/nav/helse/Routing.kt
+++ src/main/kotlin/no/nav/helse/Routing.kt
@@ -1,5 +1,6 @@
 package no.nav.helse
 
+import com.auth0.jwt.JWT
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.html.*
@@ -14,37 +15,38 @@ import no.nav.helse.auth.UserSession
 import no.nav.helse.auth.getSession
 
 fun Application.configureRouting() {
-    routing {
-        authenticate("local-stub") {
-            get("/login") {}
+  routing {
+    authenticate("local-stub") {
+      get("/login") {}
 
-            get("/callback") {
-                val principal = call.principal<OAuthAccessTokenResponse.OAuth2>()
-                principal?.state?.let { state ->
-                    call.sessions.set(UserSession(state, principal.accessToken))
-                }
-                call.respondRedirect("/home")
-            }
+      get("/callback") {
+        val principal = call.principal<OAuthAccessTokenResponse.OAuth2>()
+        principal?.state?.let { state ->
+          call.sessions.set(UserSession(state, principal.accessToken))
         }
-
-        get("/internal/health/alive") { call.respondText("alive") }
-        get("/internal/health/ready") { call.respondText("ready") }
-
-        singlePageApplication {
-            useResources = true
-            defaultPage = "index.html"
-            filesPath = "dist"
-        }
-
-        get("/") { call.respondHtml { body { p { a("/login") { +"Login" } } } } }
-
-        get("/home") {
-            val userSession: UserSession? = getSession(call)
-            if (userSession != null) {
-                call.respondText("Welcome!")
-            }
-        }
-
-        get("/login-after-fallback") { call.respondText("Redirected after fallback") }
+        call.respondRedirect("/home")
+      }
     }
+
+    get("/internal/health/alive") { call.respondText("alive") }
+    get("/internal/health/ready") { call.respondText("ready") }
+
+    singlePageApplication {
+      useResources = true
+      defaultPage = "index.html"
+      filesPath = "dist"
+    }
+
+    get("/") { call.respondHtml { body { p { a("/login") { +"Login" } } } } }
+
+    get("/home") {
+      val userSession: UserSession? = getSession(call)
+      val token = userSession?.accessToken.let { JWT.decode(it) }
+      if (userSession != null) {
+        call.respondText("Welcome ${token.subject}!")
+      }
+    }
+
+    get("/login-after-fallback") { call.respondText("Redirected after fallback") }
+  }
 }
