ce5259f1ec74af059230612861d6fc170ae717a9
Jørn-Are <ja.flaten91@gmail.com>
2026-05-13 09:00:18 +0200

    chore: use jdbc url instead of creating own url string

--- src/main/kotlin/database/Database.kt
+++ src/main/kotlin/database/Database.kt
@@ -7,21 +7,19 @@ import org.jetbrains.exposed.v1.jdbc.Database
 
 fun Application.configureDatabase() {
   val config = environment.config
-  val host = config.property("database.host").getString()
-  val port = config.property("database.port").getString()
-  val name = config.property("database.name").getString()
+  val url = config.property("database.url").getString()
   val username = config.property("database.username").getString()
   val password = config.property("database.password").getString()
 
   Flyway.configure()
-    .dataSource("jdbc:postgresql://$host:$port/$name", username, password)
+    .dataSource(url, username, password)
     .locations("classpath:db/migration")
     .load()
     .migrate()
 
   DatabaseConnection.database =
     Database.connect(
-      url = "jdbc:postgresql://$host:$port/$name",
+      url = url,
       driver = "org.postgresql.Driver",
       user = username,
       password = password,
