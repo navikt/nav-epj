103295f1db0ff5ee102a46833c4f016dd57fb8a6
Helene Arnesen <helene.arnesen@nav.no>
2026-08-28 10:53:46 +0200

    chore: ingress to epj.ekstern.dev.nav.no

--- src/main/kotlin/no/nav/helse/plugins/Cors.kt
+++ src/main/kotlin/no/nav/helse/plugins/Cors.kt
@@ -9,7 +9,7 @@ fun Application.configureCors() {
     allowHost("localhost:5173")
     allowHost("localhost:5174")
     allowHost("localhost:3000")
-    allowHost("epj.dev.nav.no")
+    allowHost("epj.ekstern.dev.nav.no")
     allowHeader(HttpHeaders.Authorization)
     allowHeader(HttpHeaders.ContentType)
     allowHeader("X-Wonderwall-Id-Token")
