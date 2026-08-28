8e4fdc0b503db2a1e08f949fa8494bcd15776e04
Helene Arnesen <helene.arnesen@nav.no>
2026-08-28 10:15:29 +0200

    chore: change ingress domain from ansatt to internal

--- src/main/kotlin/no/nav/helse/plugins/Cors.kt
+++ src/main/kotlin/no/nav/helse/plugins/Cors.kt
@@ -9,7 +9,7 @@ fun Application.configureCors() {
     allowHost("localhost:5173")
     allowHost("localhost:5174")
     allowHost("localhost:3000")
-    allowHost("epj.ansatt.dev.nav.no")
+    allowHost("epj.intern.dev.nav.no")
     allowHeader(HttpHeaders.Authorization)
     allowHeader(HttpHeaders.ContentType)
     allowHeader("X-Wonderwall-Id-Token")
