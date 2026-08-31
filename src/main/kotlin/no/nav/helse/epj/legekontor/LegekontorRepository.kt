067dd16212cceb4c683b6e008cd1670a6d7ddf9d
Helene Arnesen <86235924+helehar@users.noreply.github.com>
2026-08-31 13:08:14 +0200

    chore: inserting legekontor in route helsepersonell/me (#66)
    So we don’t have to seed the database in a V2 database migration script

--- src/main/kotlin/no/nav/helse/epj/legekontor/LegekontorRepository.kt
+++ src/main/kotlin/no/nav/helse/epj/legekontor/LegekontorRepository.kt
@@ -5,9 +5,19 @@ import no.nav.helse.core.db.LegekontorTable
 import no.nav.helse.core.db.dbQuery
 import org.jetbrains.exposed.v1.core.ResultRow
 import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.selectAll
 
 class LegekontorRepository {
+
+  suspend fun insertLegekontor(id: Uuid) = dbQuery {
+    LegekontorTable.insert {
+      it[LegekontorTable.id] = id
+      it[navn] = "navn"
+      it[tlf] = "tlf"
+      it[orgnummer] = id.toString()
+    }
+  }
 
   suspend fun findByLegekontorId(id: Uuid): Legekontor? {
     return dbQuery {
@@ -25,5 +35,9 @@ class LegekontorRepository {
       orgnummer = this[LegekontorTable.orgnummer],
       tlf = this[LegekontorTable.tlf],
     )
+  }
+
+  suspend fun legekontorInDb(id: Uuid): Boolean {
+    return dbQuery { LegekontorTable.selectAll().where { LegekontorTable.id eq id }.any() }
   }
 }
--- src/main/kotlin/no/nav/helse/epj/legekontor/LegekontorService.kt
+++ src/main/kotlin/no/nav/helse/epj/legekontor/LegekontorService.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.epj.legekontor
 
+import kotlin.uuid.Uuid
 import no.nav.helse.core.utils.LegekontorNotfoundException
 
 class LegekontorService(val legekontorRepository: LegekontorRepository) {
@@ -7,5 +8,11 @@ class LegekontorService(val legekontorRepository: LegekontorRepository) {
   suspend fun getLegekontor(legekontorId: LegekontorId): Legekontor {
     return legekontorRepository.findByLegekontorId(legekontorId.value)
       ?: throw LegekontorNotfoundException()
+  }
+
+  suspend fun insertIfNotExists(id: Uuid) {
+    if (!legekontorRepository.legekontorInDb(id)) {
+      legekontorRepository.insertLegekontor(id)
+    }
   }
 }
