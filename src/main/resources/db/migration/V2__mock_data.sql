a91b31ac622f85013b7ee412ea6e06b06bcb6a02
Helene Arnesen <helene.arnesen@nav.no>
2026-08-31 14:16:33 +0200

    chore: mock data

---
+++ src/main/resources/db/migration/V2__mock_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO helsepersonell (id, legekontor_id, hpr, navn, autorisasjon, created_at, updated_at)
+VALUES ('6453a99d-0698-430f-9d4d-d661e1d1d2c9', 'aed5c75c-3b12-4652-83d7-223bdd69062d', '565501872', 'GRØNN VITS', 'Lege', '2026-08-31 12:13:14.119450 +00:00', '2026-08-31 12:13:14.119450 +00:00');
+
+INSERT INTO pasient (id, legekontor_id, fornavn, etternavn, fnr, created_at, updated_at)
+VALUES ('d432e507-5391-4365-aeec-8475831700ee', 'aed5c75c-3b12-4652-83d7-223bdd69062d', 'MATEMATISK', 'APE', '21914897936', '2026-08-31 12:14:57.826385 +00:00', '2026-08-31 12:14:57.826385 +00:00');
