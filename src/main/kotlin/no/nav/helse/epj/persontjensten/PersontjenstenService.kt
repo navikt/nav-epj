24f004d3f08caf18d03e6c8af9f15bc4243f796e
MikAojk <joakim.taule.kartveit@nav.no>
2026-09-09 13:11:02 +0200

    chore: use env.persontjensten.baseUrl

--- src/main/kotlin/no/nav/helse/epj/persontjensten/PersontjenstenService.kt
+++ src/main/kotlin/no/nav/helse/epj/persontjensten/PersontjenstenService.kt
@@ -1,13 +1,14 @@
 package no.nav.helse.epj.persontjensten
 
+import no.nav.helse.core.Environment
 import no.nav.helse.epj.persontjensten.model.PersonName
 
-class PersontjenstenService(val baseUrl: String) {
+class PersontjenstenService(private val env: Environment) {
 
     suspend fun serachByFnr(fnr: String): PersonName? {
 
         return PersontjenstenHttpClient(
-                baseUrl,
+                env.persontjensten.baseUrl,
                 dpopToken = "21312313", // TODO this is where i need my token
             )
             .getByNin(fnr)
